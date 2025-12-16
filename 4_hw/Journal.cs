923dcaabda1a5520beda5257809d1859a8bd10ff
Elena <lentosik75@gmail.com>
2025-12-16 15:11:05 +0200

    done 4.1 hw

---
+++ 4_hw/Journal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_hw
+{
+    internal class Journal
+    {
+        /*Створіть клас "Журнал". Необхідно зберігати в полях класу:
+ * назву журналу, рік заснування, опис журналу, 
+ * контактний телефон, контактний email. 
+ * Реалізуйте методи класу для введення даних, виведення даних, 
+ * реалізуйте доступ до окремих полів через методи класу.*/
+        public string title { get; set; } = String.Empty;
+        private int year;
+        public string description { get; set; } = String.Empty;
+        public string contactPhone { get; set; } = String.Empty;
+        public string phoneNumber { get; set; } = String.Empty;
+        public string contactEmail { get; set; } = String.Empty;
+        public Journal(string title, int year, string description, string contactPhone, string phoneNumber, string contactEmail)
+        {
+            this.title = title;
+            this.year = year;
+            this.description = description;
+            this.contactPhone = contactPhone;
+            this.phoneNumber = phoneNumber;
+            this.contactEmail = contactEmail;
+        }
+        public int MyYear
+        {
+            get { return year; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Error. Wrong year data");
+                year = value;
+            }
+
+        }
+        public void show()
+        {
+            Console.WriteLine($"Name of journal: {title}, {MyYear}  year\nDescription: {description}\nContact phone: {contactPhone}" +
+                $"\nPhone number: {phoneNumber}\nContact email: {contactEmail}");
+        }
+    }
+}
---
+++ 4_hw/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _4_hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Information about your journal:");
+            Journal journal = new Journal("Vampire Diaries", 1486, "Report", "+3-488-JDBH", "+380 97 3409 233", "smth@gmail.com");
+            journal.show();
+        }
+    }
+}
