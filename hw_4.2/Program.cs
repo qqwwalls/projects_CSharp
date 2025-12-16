4eceacde69b66c318b63122384a92a9364c50982
Elena <lentosik75@gmail.com>
2025-12-16 15:31:58 +0200

    done 4.2 hw

---
+++ hw_4.2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hw_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Information about your shop: ");
+            Shop shop = new Shop("Florens","Odesa st. Tiraspolska","flowers and coffee","+380 8746 736","flow@gmail.com");
+            shop.show();
+        }
+    }
+}
---
+++ hw_4.2/Shop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_4._2
+{
+    internal class Shop
+    {
+        /*Створіть клас "Магазин". Необхідно зберігати в полях класу:
+         * назву магазину, адресу, опис профілю магазину, контактний телефон,
+         * контактний email. Реалізуйте методи класу для введення даних, виведення даних, 
+         * реалізуйте доступ до окремих полів через методи класу.*/
+        public string title { get; set; }= String.Empty;
+        public string adress { get; set; } = String.Empty;
+        public string description { get; set; } = String.Empty;
+        public string phone { get; set; } = String.Empty;
+        public string email { get; set; } = String.Empty;
+        public Shop(string title, string adress, string description, string phone, string email)
+        {
+            this.title = title;
+            this.adress = adress;
+            this.description = description;
+            this.phone = phone;
+            this.email = email;
+        }
+        public void show()
+        {
+         Console.WriteLine($"Title: {title}\nAdress: {adress}\nDescription: {description}\nPhone: {phone}\nEmail: {email}");
+        }
+    }
+}
