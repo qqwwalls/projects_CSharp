9859d28e33bf85e27caf02430223b5eae3da8ec3
Elena <lentosik75@gmail.com>
2025-12-16 16:40:24 +0200

    done 6 hw

---
+++ hw_6/Employee.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_6
+{
+
+    internal class Employee
+    {
+        public string MyName { get; set; } = String.Empty;
+        private int age;
+        private int salary;
+
+        public Employee(string name, int age, int salary)
+        {
+            this.MyName = name;
+            this.MyAge = age;
+            this.MySalary = salary;
+        }
+
+        public int MyAge
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 || value > 120)
+                    throw new ArgumentException("Error. Wrong age");
+                age = value;
+            }
+
+        }
+        public int MySalary
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Error. Wrong salary");
+                salary = value;
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("Name: " + MyName + ", Age: " + MyAge + ", Salary: " + MySalary);
+        }
+        // перевантаження операторів
+        public static Employee operator +(Employee emp, int value)
+        {
+            emp.MySalary += value;
+            return emp;
+        }
+
+        public static Employee operator -(Employee emp, int value)
+        {
+            emp.MySalary -= value;
+            return emp;
+        }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.MySalary == emp2.MySalary;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.MySalary != emp2.MySalary;
+        }
+
+        public static bool operator >(Employee emp1, Employee emp2)
+        {
+            return emp1.MySalary > emp2.MySalary;
+        }
+
+        public static bool operator <(Employee emp1, Employee emp2)
+        {
+            return emp1.MySalary < emp2.MySalary;
+        }
+        // додаткове перевизначення
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee emp)
+                return this.MySalary == emp.MySalary;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return MySalary.GetHashCode();
+        }
+    }
+
+    }
---
+++ hw_6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace hw_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Додайте до вже створеного класу інформацію про заробітну плату співробітника. 
+             Виконайте перевантаження + (для збільшення зарплати на зазначену величину), 
+              - (для зменшення зарплати на зазначену величину), 
+             == (перевірка на рівність зарплат працівників), 
+             != ,Порівняйте заробітні платні двох спіробітників empl1>empl2 та empl1<empl2
+             Використовуйте механізм властивостей для полів класу.*/
+            Employee worker1 = new Employee("Oskar", 23, 5000);
+            worker1.show();
+            Employee worker2 = new Employee("Nina", 28, 7000);
+            worker2.show();
+            worker1 = worker1 + 600;   
+            worker2 = worker2 - 1200;    
+
+            Console.WriteLine(worker1 > worker2); 
+            Console.WriteLine(worker1 < worker2); 
+            Console.WriteLine(worker1 == worker2); 
+            Console.WriteLine(worker1 != worker2); 
+        }
+    }
+}
