16b1957fe3a387d77fc708575ababd29d1b34a22
Elena <lentosik75@gmail.com>
2026-01-07 22:30:12 +0200

    done hw_8.1

---
+++ hw_8/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace hw_8
+{
+
+    internal class Program
+
+    {
+        public delegate bool NumberFilter(int number);
+
+        static int[] FilterArray(int[] array, NumberFilter filter)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int number in array)
+            {
+                if (filter(number))
+                    result.Add(number);
+            }
+
+            return result.ToArray();
+        }
+
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool IsFibonacci(int number)
+        {
+            if (number < 0)
+                return false;
+
+            int a = 0;
+            int b = 1;
+
+            while (a < number)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return a == number;
+        }
+
+        static void PrintArray(string title, int[] array)
+        {
+            Console.Write(title + ": ");
+            foreach (int n in array)
+                Console.Write(n + " ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 13, 21, 22 };
+
+            PrintArray("Even", FilterArray(numbers, IsEven));
+            PrintArray("Odd", FilterArray(numbers, IsOdd));
+            PrintArray("Prime", FilterArray(numbers, IsPrime));
+            PrintArray("Fibonacci", FilterArray(numbers, IsFibonacci));
+        }
+    }
+}
