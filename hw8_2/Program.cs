b18ce41bc3d9dd2c4de82f7db65cfe909d951bbe
Elena <lentosik75@gmail.com>
2026-01-07 22:42:32 +0200

    done hw8_2

---
+++ hw8_2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace hw8_2
+{
+    internal class Program
+    {
+        public delegate int StringOperation(string text);
+
+        static int CountVowels(string text)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+
+            foreach (char c in text)
+            {
+                if (vowels.Contains(c))
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int CountConsonants(string text)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c) && !vowels.Contains(c))
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int GetLength(string text)
+        {
+            return text.Length;
+        }
+        static void Main(string[] args)
+        {
+            string words = "Banana mit Apfel";
+
+            StringOperation operation;
+
+            operation = CountVowels;
+            Console.WriteLine("Vowels: " + operation(words));
+
+            operation = CountConsonants;
+            Console.WriteLine("Consonants: " + operation(words));
+
+            operation = GetLength;
+            Console.WriteLine("Length: " + operation(words));
+        }
+    }
+}
